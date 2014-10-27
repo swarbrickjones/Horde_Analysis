ba04ca6c232708db4024ba2dc1efd3762a5fedee
Mike Swarbrick Jones <m.swarbrickjone@ocado.com>
2014-10-27 17:49:50 +0000

    iPython work etc

---
+++ analysis.py
@@ -0,0 +1,84 @@
+import os
+import pandas as pd
+import matplotlib
+from matplotlib import pyplot as plt
+import numpy as np
+
+font = {'family' : 'sans-serif',
+        'weight' : 'bold',
+        'size'   : 10}
+
+matplotlib.rc('font', **font)
+
+__location__ = os.path.realpath((os.getcwd()))
+
+raw_data = pd.read_csv(os.path.join(__location__, 'cleaned_data.csv'))
+raw_data['date_time']  = pd.to_datetime(raw_data['date_time'])
+
+user_messages = raw_data[raw_data['user']!='WhatsApp']
+notifications = raw_data[raw_data['user']=='WhatsApp']
+
+
+min_day = raw_data['date_time'].min()
+max_day = raw_data['date_time'].max()
+
+join_notifications = notifications[[string[0:15] == 'USER_ENTERED : ' for string in notifications['message']]]
+left_notifications = notifications[[string[0:12] == 'USER_LEFT : ' for string in notifications['message']]]
+
+joined_dict={}
+left_dict={}
+
+
+def add_notification_to_dict(dictionary, table, chars_to_skip):
+    for row in table.iterrows():
+        user = row[1].get_value('message')[chars_to_skip:]
+        date = row[1].get_value('date_time')
+        if not dictionary.has_key(user):
+            dictionary[user]=list()  
+        dictionary[user].append(date)
+
+add_notification_to_dict(joined_dict, join_notifications, 15)
+add_notification_to_dict(left_dict, left_notifications, 12)
+
+user_date_ranges = {}
+  
+for user in joined_dict.keys():
+    if not left_dict.has_key(user):
+        left_dict[user] = [max_day]
+    joined_list = joined_dict[user]  
+    left_list = left_dict[user]
+    if len(joined_list) > len(left_list):
+        left_list.append(max_day)
+    user_date_ranges[user]=[]
+    for index in range(len(joined_list)):
+        dr = pd.date_range(joined_list[index], left_list[index])
+        user_date_ranges[user].append(dr)
+
+days = []
+user_counts = []
+#
+#for day in pd.date_range(start=min_day,end = max_day - pd.DateOffset(days =1),freq='3H'):
+#    days.append(day)
+#    count = 0
+#    for user in user_date_ranges.keys():
+#        for date_range in user_date_ranges[user]:
+#            if day > date_range.min() and day <= date_range.max():
+#                count += 1
+#    user_counts.append(count)
+#    
+#
+#fig1 = plt.plot(days, user_counts)
+#plt.show(fig1)
+
+one_day = np.timedelta64(1, 'D')
+days_active_map = []
+for user in user_names:
+    total= 0
+    for date_range in user_date_ranges[user]:
+        total += (date_range.max() - date_range.min()).days
+    return total
+    
+
+    
+
+
--- data_munging.py
+++ data_munging.py
@@ -9,20 +9,68 @@ __location__ = os.path.realpath(
 
 def split_line(previous_time, previous_user, input_text):
     date_split = input_text.split(' - ',1)
-    if len(date_split) == 1:
+    if len(date_split) == 1:        
         return previous_time, previous_user, input_text
-    time_string = get_date( date_split[0] )
+    time_string = ""
+    try :
+        time_string = get_date( date_split[0] )
+    except ValueError:
+            return previous_time, previous_user, input_text
     message_split = date_split[1].split(": ",1)
     if len(message_split) == 1 :
-        return time_string, "WHATSAPP", date_split[1]
-    return time_string, message_split[0], message_split[1]
+        return time_string, 'WhatsApp', date_split[1]
+    return time_string, user_name_map[message_split[0]], message_split[1]
     
+input_date_format = '%H:%M, %d %b %Y'
+
+output_date_format = '%H:%M, %d %b %Y'
+
+user_name_map = {    \
+    'Eliza Botts' : 'Eliza', \
+    'Cate':'Cate', \
+    '\xe2\x80\xaa+44 7807 744465\xe2\x80\xac':'Hannah', \
+    'Kritzia':'Krizia', \
+    'Wills':'Wills',  \
+    'Angela':'Angela',  \
+    'Bunn':'Bunn',  \
+    '\xe2\x80\xaa+44 7979 853510\xe2\x80\xac':'Chris', \
+    'Dave Mendez':'Dave', \
+    '\xe2\x80\xaa+44 7794 501335\xe2\x80\xac':'Catrin', \
+    '\xe2\x80\xaa+44 7545 989149\xe2\x80\xac':'Dan',  \
+    'Teegan':'Tegan', \
+    'Mike':'Mike', \
+    'Joe Jarman':'Joe', \
+    'Alan Wright':'Alan', \
+    'p tschismarov':'Phil', \
+    'WhatsApp':'WhatsApp', \
+    'Henry Franks':'Henry', \
+    '\xe2\x80\xaa+61 405 567 873\xe2\x80\xac':'Wagg', \
+    'Mateusz Tylicki':'Mateusz', \
+    'Marth':'Marth', \
+    'Lora Staffenschloten' : 'Lora', \
+    'Bongo':'Bongo', \
+    '\xe2\x80\xaa+44 7830 332317\xe2\x80\xac':'Simone', \
+    'Shaggy Crazy Horse':'Shaggy', \
+    '\xe2\x80\xaa+44 7834 379555\xe2\x80\xac':'Vinay', \
+    'Owen James':'Owen', \
+    'Slam':'Sam', \
+    '\xe2\x80\xaa+44 7754 268447\xe2\x80\xac':'Cate', \
+    '\xe2\x80\xaa+44 7502 048124\xe2\x80\xac':'Lorna', \
+    'Vanessa "Beast" Matthews':'Vanessa', \
+    'Adamina Carden':'Adimina', \
+    '\xe2\x80\xaa+44 7944 609055\xe2\x80\xac':'Em', \
+    '\xe2\x80\xaa+44 7850 688790\xe2\x80\xac':'Sadie'
+    }
+
+
+
+
 def get_date ( input_string ):
     date = None
     try:
-        date = time.strptime(input_string,'%d %b %Y %H:%M')
+        date = time.strptime(input_string,input_date_format)
     except ValueError:
-        date = time.strptime(input_string + ' 2014','%d %b %H:%M %Y')
+        date = time.strptime(input_string + ' 2014',input_date_format)
     return time.strftime('%d %b %H:%M %Y',date)
         
 def process_body(line):
@@ -34,10 +82,25 @@ def get_cleaned_line(date_time, user, body):
     return date_time + ":::" + user + ":::" + process_body(body) + "\n"
     
     
+def format_notification(message):
+    for key in user_name_map.keys():
+        message = message.replace(key, user_name_map[key])
+    if message[0:12] == "Shaggy added":
+        message = "USER_ENTERED : " + message[13 :]
+    if message[-6:] == "joined":
+        message = "USER_ENTERED : " + message[:-7]
+    if message[0:14] == "Shaggy removed":
+        message = "USER_LEFT : " + message[15 :]
+    if message[-4:] == "left":
+        message = "USER_LEFT : " + message[:-5]
+    if message[-11:] == "was removed":
+        message = "USER_LEFT : " + message[:-12]
+    return message
+                
 
-raw_data =  open(os.path.join(__location__, 'raw_data.txt'),'r')
+raw_data =  open(os.path.join(__location__, 'phil_raw.txt'),'r')
 
-cleaned_data =  open(os.path.join(__location__, 'cleaned_data.txt'),'w')
+##cleaned_data =  open(os.path.join(__location__, 'phil_raw.txt'),'w')
 
 cleaned_df = DataFrame(columns=('date_time', 'user', 'message'))
 
@@ -48,30 +111,29 @@ print len(lines)
 df_index = 0
 count = 0
 
-current_date_time = "--"
-current_user = "--"
-current_message = "--"
+current_date_time = "date_time"
+current_user = "user"
+current_message = "message"
 
 for line_raw in lines:
     line= line_raw.rstrip('\n')
     date_time,user,message = split_line(current_date_time, current_user, line)
-    if user.__eq__(current_user):
-        current_message = current_message + " \\ " +message
+    if user.__eq__(current_user) and current_user != 'WhatsApp':
+        current_message = current_message + " // " + process_body(message)
+        continue
     else:
+        if user == 'WhatsApp':    
+            message = format_notification(message)            
         cleaned_df.loc[df_index] = [current_date_time, current_user, current_message]
         current_date_time = date_time
         current_user = user
-        current_message = message
+        current_message = process_body(message)
         df_index += 1
-    if count % 100 == 0:
+    if count % 1000 == 0:
         print count
     count += 1
    
-output_path = os.path.join(__location__, 'cleaned_data.txt')   
-cleaned_df.to_csv(output_path, index = False, quoting = csv.QUOTE_ALL)
-    
-
-
-cleaned_data.close()
+output_path = os.path.join(__location__, 'cleaned_data.csv')
+cleaned_df.to_csv(output_path, index = False, quoting = csv.QUOTE_ALL, header = False)
     
 
